e724ab760de3e2f0bf0b0c33da494c12bcb72dae
Leider Bautista <leiderbautista1999@gmail.com>
2022-07-14 09:57:36 -0500

    documento readme

--- config/db.php
+++ config/db.php
@@ -2,6 +2,7 @@
 
 class Database{
     public static function connect(){
+        // $db= new mysqli('puntodondeseencutralabasededatos', 'nombre de usuario', 'contraseñabasededatos', 'nombreDB');
         $db= new mysqli('localhost', 'root', '', 'Acme');
         $db->query("SET NAMES 'utf8'");
         return $db;
